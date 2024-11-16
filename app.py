4a33ab4cee4442ac1a4d5b3af2625504c7ae82a0
Tanmay Shinde <101259685+Tanny28@users.noreply.github.com>
2024-11-16 10:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ import streamlit.components.v1 as components  # Import Streamlit components
 # Page Configuration
 st.set_page_config(page_title="Disease Prediction Dashboard", layout="wide", page_icon="🩺")
 
-# Enlarged Canva Embed Code
+# Updated Canva Embed Code
 components.html(
     """
-    <div style="position: relative; width: 90%; height: 0; padding-top: 70%; 
-    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 1.6em; overflow: hidden;
-    border-radius: 8px; will-change: transform; margin-left: auto; margin-right: auto;">
-        <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
-          src="https://www.canva.com/design/DAGUIKuxKWQ/4ieiuxWMEWG164U4N80sNw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+    border-radius: 8px; will-change: transform;">
+        <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+          src="https://www.canva.com/design/DAGWnb97gtE/cyRhaMPCQMsU4BVn2uVU1Q/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
         </iframe>
     </div>
-    <a href="https://www.canva.com/design/DAGUIKuxKWQ/4ieiuxWMEWG164U4N80sNw/view?utm_content=DAGUIKuxKWQ&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Employee Field Guide</a> by Tanmay Shinde
+    <a href="https://www.canva.com/design/DAGWnb97gtE/cyRhaMPCQMsU4BVn2uVU1Q/view?utm_content=DAGWnb97gtE&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Epidemics vs. Pandemics Quiz Presentation in White Photographic Style</a> by Tanmay Shinde
     """,
     height=600,  # Adjust overall height as needed
 )
